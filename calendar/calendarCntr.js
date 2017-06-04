ff433ff4bc10868a92f3b4a6159161cab020e07c
alexsominskiy <sominskiy@gmail.com>
2017-06-04 12:44:35 +0300

    second commit

--- calendar/calendarCntr.js
+++ calendar/calendarCntr.js
@@ -117,8 +117,7 @@ app.controller("calendarCntr",function($scope, $http){
     var hcode = [0, 10, 72.5];
     $scope.scrollContainer = function(code){
         var cont = document.getElementById("daysContainer");
-        console.log($(window).height*hcode[code]/100+";"+(currMoment.date() - 1));
-        cont.scrollTop = $(window).height*hcode[code]/100  * (currMoment.date() - 1);
+        cont.scrollTop = $(window).height()*hcode[code]/100  * (currMoment.date() - 1);
     }
     
     
