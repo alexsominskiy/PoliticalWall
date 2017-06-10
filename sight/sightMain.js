6bae4d74485ff84fcff91c41edb276a73731c471
alexsominskiy <sominskiy@gmail.com>
2017-06-10 12:46:40 +0300

    second commit

--- sight/sightMain.js
+++ sight/sightMain.js
@@ -3,7 +3,7 @@
 var url ="https://politicalwallproject.herokuapp.com";
 
 var app = angular.module("politicsMain",[]);
-app.controller("politicsMainCntr",['betweenFilter',function($scope,$window,$http){
+app.controller("politicsMainCntr",function($scope,$window,$http){
     
     $scope.hebVocab = vocab;
 
@@ -183,7 +183,7 @@ app.controller("politicsMainCntr",['betweenFilter',function($scope,$window,$http
         $scope.main = false;
         $scope.page = "sight/SightCharts/charts.html";
     }
-}])
+})
     .directive("skewlabelright", function(){
         return {
             template: '<div class="wrap pointer">'+
