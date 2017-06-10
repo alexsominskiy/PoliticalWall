be1fd6c6e9d0763b5c8c55d58a00e0c71378779f
alexsominskiy <sominskiy@gmail.com>
2017-06-10 12:10:14 +0300

    second commit

--- news/uploadedNewsCntr.js
+++ news/uploadedNewsCntr.js
@@ -1,7 +1,7 @@
 /**
  * Created by User on 23.03.2017.
  */
-app.controller("uploadedNewsCntr", ['betweenFilter',function($scope,$http) {
+app.controller("uploadedNewsCntr", function($scope,$http) {
     $scope.back = function () {
         $scope.model.page = "news/news.html";
         $scope.model.headers = headers.news;
@@ -64,7 +64,7 @@ app.controller("uploadedNewsCntr", ['betweenFilter',function($scope,$http) {
     
     $scope.dateBefore = "";
     $scope.dateAfter = "";
-}])
+})
 .filter("between",function($scope){return function(arr){
     console.log(moment($scope.dateBefore,"DD/MM/YYYY"));
     return arr;
