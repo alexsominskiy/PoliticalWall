465e1c9faa2e2f00073c4b1987f8b3cf7a3a3056
alexsominskiy <sominskiy@gmail.com>
2017-06-10 14:26:40 +0300

    second commit

--- news/uploadedNewsCntr.js
+++ news/uploadedNewsCntr.js
@@ -53,8 +53,8 @@ app.controller("uploadedNewsCntr", function($scope,$http) {
 app.filter('dateRange', function() {
         return function(items, startDate, endDate) {
             
-            var sDate = moment(startDate).format("DD/MM/YYYY") || moment("01/01/1900").format("DD/MM/YYYY");
-            var eDate = moment(endDate).format("DD/MM/YYYY") || moment("01/01/2100").format("DD/MM/YYYY");
+            var sDate = moment(startDate,"DD/MM/YYYY") || moment("01/01/1900","DD/MM/YYYY");
+            var eDate = moment(endDate,"DD/MM/YYYY") || moment("01/01/2100","DD/MM/YYYY");
             
             console.log(startDate);
 
