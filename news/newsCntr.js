64571203c651525f6e3877e9b6c0c3a07c44b45c
alexsominskiy <sominskiy@gmail.com>
2017-06-17 14:29:02 +0300

    second commit

--- news/newsCntr.js
+++ news/newsCntr.js
@@ -7,6 +7,7 @@ app.controller("newsCntr",function($scope,$http,$sce){
     
     $scope.articleDate = moment();
     $("#articleDate").datepicker($.datepicker.regional["he"]);
+    $scope.articleDate = new Date();
     
     $scope.shieldMessage1 = "news";
     $scope.showShield = function(){
