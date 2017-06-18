78aeeca45610faa0e7cd4d837bf1cc705b15c87c
alexsominskiy <sominskiy@gmail.com>
2017-06-18 14:08:59 +0300

    second commit

--- backend.js
+++ backend.js
@@ -162,7 +162,7 @@ app.get("/uploadNews", function(req,res) {
         "'"+fauthor+"',"+
         "'"+ftitle+"',"+
         "'"+fdate+"',"+
-        "'"+"לא"+"'"+
+        "'"+currentArticle.subjectText.substring(0,100)+"'"+
     ")";
     res.write(dbReq);
     connection.query(dbReq, function(err){
